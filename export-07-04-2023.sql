5d99b9e16530467e6dfbdb73e2716ddb231f3305
Nobin <nobin.j@holosuit.com>
2023-04-07 22:03:20 +0530

    updated database schema

--- export-07-04-2023.sql
+++ export-07-04-2023.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `blogs` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `blogs`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: blogs
@@ -73,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-07 21:56:45
+-- Dump completed on 2023-04-07 22:02:39
